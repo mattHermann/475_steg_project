a9aaf103a11392a5170f33bc9837085503b476b6
Matthew <hermmatt@umich.edu>
2020-04-19 01:59:45 -0400

    Implement encode function

--- Image_LSB.py
+++ Image_LSB.py
@@ -1,7 +1,18 @@
 from PIL import Image, ImageFilter
 import sys, getopt
 import numpy as np
+import math
 
+
+def get_new_color(color, msg_bytes, bit_index, total_bits):
+	#encode LSB if the entire message has not been encoded
+	if (bit_index < total_bits):
+		color_bin = bin(color)
+		new_color_bin = color_bin[:-1] + msg_bytes[bit_index]
+		bit_index += 1
+		return int(new_color_bin, 2), bit_index
+	else:
+		return color, bit_index
 
 
 
@@ -11,31 +22,39 @@ def encode(img):
 	msg_bytes = ''.join(format(ord(i), 'b') for i in plaintext_msg) 
 	print(msg_bytes)
 
-	byte_index = 0
-	total_bytes = len(msg_bytes)
+	bit_index = 0
+	total_bits = len(msg_bytes)
 
 	width, height = img.size
 
+
+	if (total_bits > (width * height * 3)):
+		max_msg_size = math.floor((width * height * 3) / 7)
+		print("Message is too long to encode, Max message size for this image is:", max_msg_size, "characters")
+		sys.exit(2)
+
+	pixel_arr = img.load()
+
 	stego_im = Image.new(img.mode, img.size)
 	stego_pixels = stego_im.load()
-	pixel_arr = img.load()
+
+	
 
 	for i in range(width):
 		for j in range(height):
-			if (byte_index < total_bytes):
-				print(pixel_arr[i,j])
-				byte_index += 1
-				
+			r,g,b = pixel_arr[i,j]
 
-			else:
-				stego_pixels[i,j] = pixel_arr[i,j]
+			new_r, bit_index = get_new_color(r, msg_bytes, bit_index, total_bits)
+			new_g, bit_index = get_new_color(g, msg_bytes, bit_index, total_bits)
+			new_b, bit_index = get_new_color(b, msg_bytes, bit_index, total_bits)
+
+			stego_pixels[i,j] = (new_r, new_g, new_b)
 	
-	stego_im.save('test.jpg')
-
-	print("encode")
+	stego_im.save('steg.jpg')
 
 def decode(img):
-	pix_val = list(img.getdata())
+	width, height = img.size
+	pixel_arr = img.load()
 
 	#decoding data for binary string
 	decoded_str = ''
