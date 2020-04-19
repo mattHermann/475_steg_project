f0d3703a817520e52a61e4d7e0b8f9aebb231245
brsmythe <brsmythe@umich.edu>
2020-04-19 18:02:43 -0400

    Padded message correclty

--- Image_LSB.py
+++ Image_LSB.py
@@ -10,132 +10,138 @@ import math
 
 
 def otp_encrypt(plaintext_msg):
-	print(plaintext_msg)
-	alphabet = string.ascii_letters #string.digits + string.punctuation
-	key = ''.join(secrets.choice(alphabet) for i in range(len(plaintext_msg)))
+    print(plaintext_msg)
+    alphabet = string.ascii_letters #string.digits + string.punctuation
+    key = ''.join(secrets.choice(alphabet) for i in range(len(plaintext_msg)))
 
 
-	encrypted_msg = ''
+    encrypted_msg = ''
 
-	for i in range(len(plaintext_msg)):
-		c_bit = ord(plaintext_msg[i]) ^ ord(key[i])
-		encrypted_msg += chr(c_bit)
+    for i in range(len(plaintext_msg)):
+        c_bit = ord(plaintext_msg[i]) ^ ord(key[i])
+        encrypted_msg += chr(c_bit)
 
-	print(key, "with length", len(encrypted_msg))
-	print(encrypted_msg, "with length", len(encrypted_msg))
+    print(key, "with length", len(encrypted_msg))
+    print(encrypted_msg, "with length", len(encrypted_msg))
 
-	return encrypted_msg, key
+    return encrypted_msg, key
 
 def otp_decrypt(otp_msg, key):
-	decrypted_msg = ''
+    decrypted_msg = ''
 
 
-	for i in range(len(key)):
-		c_bit = ord(otp_msg[i]) ^ ord(key[i])
-		decrypted_msg += chr(c_bit) 
-	
-	return decrypted_msg
+    for i in range(len(key)):
+        c_bit = ord(otp_msg[i]) ^ ord(key[i])
+        decrypted_msg += chr(c_bit)
+    
+    return decrypted_msg
 
 
 
 def get_new_color(color, msg_bits, bit_index, total_bits):
-	#encode LSB if the entire message has not been encoded
-	if (bit_index < total_bits):
-		color_bin = bin(color)
-		new_color_bin = color_bin[:-1] + msg_bits[bit_index]
-		bit_index += 1
-		return int(new_color_bin, 2), bit_index
-	else:
-		return color, bit_index
+    #encode LSB if the entire message has not been encoded
+    if (bit_index < total_bits):
+        color_bin = bin(color)
+        new_color_bin = color_bin[:-1] + msg_bits[bit_index]
+        bit_index += 1
+        return int(new_color_bin, 2), bit_index
+    else:
+        return color, bit_index
+
+def pad_zeros(input):
+    if len(input) < 7:
+        input = '0'*(7-len(input))+input
+    return input
 
 def encode(img):
-	plaintext_msg = input('>Please type the message your wish to enocde: ')
-	otp_msg, key = otp_encrypt(plaintext_msg) 
+    plaintext_msg = input('>Please type the message your wish to encode: ')
+    otp_msg, key = otp_encrypt(plaintext_msg)
 
-	msg_bits = ''.join(format(ord(i), 'b') for i in otp_msg) 
-	print(msg_bits)
-	print(''.join(format(ord(i), 'b') for i in key))
-	print(''.join(format(ord(i), 'b') for i in plaintext_msg))
-	
+    msg_bits = ''.join(pad_zeros(format(ord(i), 'b')) for i in otp_msg)
+    print(msg_bits)
+    print(''.join(format(ord(i), 'b') for i in key))
+    print(''.join(format(ord(i), 'b') for i in plaintext_msg))
+    
 
-	bit_index = 0
-	total_bits = len(msg_bits)
+    bit_index = 0
+    total_bits = len(msg_bits)
 
-	width, height = img.size
+    width, height = img.size
 
 
-	if (total_bits > (width * height * 3)):
-		max_msg_size = math.floor((width * height * 3) / 7)
-		print("Message is too long to encode, Max message size for this image is:", max_msg_size, "characters")
-		sys.exit(2)
+    if (total_bits > (width * height * 3)):
+        max_msg_size = math.floor((width * height * 3) / 7)
+        print("Message is too long to encode, Max message size for this image is:", max_msg_size, "characters")
+        sys.exit(2)
 
-	pixel_arr = img.load()
+    pixel_arr = img.load()
 
-	stego_im = Image.new(img.mode, img.size)
-	stego_pixels = stego_im.load()
+    stego_im = Image.new(img.mode, img.size)
+    stego_pixels = stego_im.load()
 
-	for i in range(width):
-		for j in range(height):
-			r,g,b = pixel_arr[i,j]
+    for i in range(width):
+        for j in range(height):
+            r,g,b = pixel_arr[i,j]
 
-			new_r, bit_index = get_new_color(r, msg_bits, bit_index, total_bits)
-			new_g, bit_index = get_new_color(g, msg_bits, bit_index, total_bits)
-			new_b, bit_index = get_new_color(b, msg_bits, bit_index, total_bits)
+            new_r, bit_index = get_new_color(r, msg_bits, bit_index, total_bits)
+            new_g, bit_index = get_new_color(g, msg_bits, bit_index, total_bits)
+            new_b, bit_index = get_new_color(b, msg_bits, bit_index, total_bits)
 
-			stego_pixels[i,j] = (new_r, new_g, new_b)
-	
-	#print("Success! The encryption key is:", key)
-	stego_im.save('steg.png')
+            stego_pixels[i,j] = (new_r, new_g, new_b)
+    
+    #print("Success! The encryption key is:", key)
+    stego_im.save('steg.png')
 
 def get_LSB(color):
-	color_bin = bin(color)
-	return color_bin[-1]
+    color_bin = bin(color)
+    return color_bin[-1]
 
 def decode(img):
-	width, height = img.size
-	pixel_arr = img.load()
+    width, height = img.size
+    pixel_arr = img.load()
 
-	msg_bits = ''
+    msg_bits = ''
 
-	for i in range(width):
-		for j in range(height):
-			r,g,b = pixel_arr[i,j]
-			msg_bits += get_LSB(r)
-			msg_bits += get_LSB(g)
-			msg_bits += get_LSB(b)
+    for i in range(width):
+        for j in range(height):
+            r,g,b = pixel_arr[i,j]
+            msg_bits += get_LSB(r)
+            msg_bits += get_LSB(g)
+            msg_bits += get_LSB(b)
 
-	#decoding data for binary string
-	otp_str = ''
-	for i in range(0, len(msg_bits), 7): 
-		split_bin_data = msg_bits[i:i + 7] 
-		split_dec = int(split_bin_data, 2) 
-		otp_str = otp_str + chr(split_dec)  
+    #decoding data for binary string
+    otp_str = ''
+    for i in range(0, len(msg_bits), 7):
+        split_bin_data = msg_bits[i:i + 7]
+        split_dec = int(split_bin_data, 2)
+        otp_str = otp_str + chr(split_dec)
 
-	#key = input(">Please enter the encryption key: ")
-	#decrypted_msg = otp_decrypt(otp_str, key)
+    #key = input(">Please enter the encryption key: ")
+    #decrypted_msg = otp_decrypt(otp_str, key)
 
 
-	print("The decoded string is:", otp_str[:10])
+    print("The decoded string is:", otp_str[:10])
 
 
 def main(argv):
-	input_image = ''
-	output_file = ''
-	if( len(sys.argv) != 2 ):
-		print("USAGE: Image_LSB.py <input_image>")
-		sys.exit(2)
+    input_image = ''
+    output_file = ''
+    if( len(sys.argv) != 2 ):
+        print("USAGE: Image_LSB.py <input_image>")
+        sys.exit(2)
 
-	input_file = sys.argv[1]
-	img = Image.open(input_file, 'r')
+    input_file = sys.argv[1]
+    img = Image.open(input_file, 'r')
 
-	mode_num = input('>Which Mode? 1) encode 2) decode: ') 
-	if(mode_num == '1'):
-		encode(img)
-	elif(mode_num == '2'):
-		decode(img)
-	else:
-		print("Invalid Mode Number")
-		sys.exit(2)
+    mode_num = input('>Which Mode? 1) encode 2) decode: ')
+    if(mode_num == '1'):
+        encode(img)
+    elif(mode_num == '2'):
+        decode(img)
+    else:
+        print("Invalid Mode Number")
+        sys.exit(2)
 
 if __name__=="__main__":
     main(sys.argv[1:])
+
